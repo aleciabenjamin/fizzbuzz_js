1b8ec796108fc738de6fa70ec16fe9ec49b4a325
Alecia <aleciabenjamin@gmail.com>
2019-02-02 21:06:54 +0100

    create fizz-buzz.spec.js and pass first 2 tests

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -1,4 +1,4 @@
-require('../spec.helper')
+require('./spec.helper')
 
 describe('Your test case description', () => {
     // Setup
@@ -6,13 +6,13 @@ describe('Your test case description', () => {
 
     beforeEach(() => {
         // assign values to your variables
-        array = new Array(2, 3)
-    });
+        array = new Array(2, 3);
+    })
 
     it('add a descriptive test title', () => {
         // Execute code if needed
         const sum = array[0] + array[1]
         // add an assertion using the `expect` keyword
-        expect(sum).to.eql(5)
+        expect(sum).to.eql(5);
     })
 })
